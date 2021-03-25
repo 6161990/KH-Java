c3a395e81938765b1196bbd0a67cff57d09dc8d7
6161990 <6161990src@gmail.com>
2021-03-26 06:09:09 +0900

    Day20210325

---
+++ Day20210325/src/kr/or/kh/ArrayTest3.java
@@ -0,0 +1,18 @@
+package kr.or.kh;
+
+import java.util.Scanner;
+
+public class ArrayTest3 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		char[] ju = new char[2];
+//		ju[0]='y';
+//		ju[1]='r';
+		System.out.println("한 문자 입력");
+		ju[0]=input.next().charAt(0);
+		System.out.println("한 문자 입력");
+		ju[1]=input.next().charAt(0);
+	}
+
+}

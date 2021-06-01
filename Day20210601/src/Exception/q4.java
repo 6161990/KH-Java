d7a6cb4fde7d7bccb2b0df557084318ea09f44d1
'6161990' <'6161990src@gmail.com>
2021-06-01 16:56:18 +0900

    자바 테스트 4번문제 String[] args 이용해 파일 읽고 쓰기

--- Day20210601/src/Exception/q4.java
+++ Day20210601/src/Exception/q4.java
@@ -1,5 +1,31 @@
 package Exception;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class q4 {
+	public static void main(String[] args) {
+		
+	  try {	
+		FileReader file = new FileReader(args[0]); // city\\city.txt 
+		BufferedReader buff = new BufferedReader(file);
+		FileWriter writer = new FileWriter(args[1]); // city\\city2.txt
+		String text;
+		while((text=buff.readLine())!=null) {
+			System.out.println(text);
+			writer.write(text);
+			writer.write("\r\n");
+		}
+		file.close();
+		writer.close();
+	  }catch(FileNotFoundException e) {
+		  e.printStackTrace();
+	  }catch(IOException e) {
+		  e.printStackTrace();
+	  }
+	}
 
 }
